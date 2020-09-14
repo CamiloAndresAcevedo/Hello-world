0c0bf6544292ba8c956544d28106d79833f8af5a
Camilo Andres <Camilo.acevedo.2019@upb.edu.co>
2020-09-14 15:59:18 -0500

    Este es un programa que imprime un hello world utilizando java

---
+++ hello_world.java
@@ -0,0 +1,15 @@
+package Laboratorios;
+
+public class hello_world {
+
+	public static void main(String[] args) {
+//Este es un programa que imprime un hello world.
+//Realizado por Camilo Andres Acevedo.
+		
+		System.out.println("Hello world");
+		System.out.println("Hola mundo, que tenga un buen dia.");
+
+
+	}
+
+}
